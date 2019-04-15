7f0b1f7ece847c968752ce4c4221003219c27534
Giovanni Rodrigues De Morais <38850490828@info.local>
2019-04-15 10:23:20 -0300

    OO

---
+++ Ex2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----Bem Vindo ao SENAIzinho-----");
+
+            Console.WriteLine();
+        }
+    }
+}
